777a35a0489495d28ad263c5c1c8861509e148e9
AryaBhole <bholeaarya@gmail.com>
2026-08-18 01:25:38 +0530

    v6 set nullable to false again doesnt change any functionality as it was backed up already

--- src/main/java/com/arya/attendance/model/AttendanceRecord.java
+++ src/main/java/com/arya/attendance/model/AttendanceRecord.java
@@ -18,7 +18,7 @@ public class AttendanceRecord {
     private Course course;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
